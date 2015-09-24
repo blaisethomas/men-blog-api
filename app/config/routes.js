f6132e816cd9982a17adee6ad7442313fb38328e
Blaise Thomas <blaisegthomas@gmail.com>
2015-09-24 13:05:09 -0700

    builds schema for article

---
+++ app/config/routes.js
@@ -0,0 +1,9 @@
+var express = require('express'),
+		apiRouter = express.Router(),
+		articlesController = require('../controllers/articles'),
+		Article = require('../models/article');
+
+apiRouter.route('/articles')
+	.get()
+	.post();
+
